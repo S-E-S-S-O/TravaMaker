464263221b262d16f798684bd47aa8844d2f62b9
Takaso <70639611+Takaso@users.noreply.github.com>
2021-10-26 18:33:10 +0200

    Update TravaMaker.py

--- TravaMaker.py
+++ TravaMaker.py
@@ -8,7 +8,7 @@ class MOD_HACKED_MALWARE():
    def hacking(hacked):
       print("Generating trava...")
       for i in range(4):
-        print(i + "...")
+        print(str(i) + "...")
       print("ERROR 404 TRAVA HACKED!")
       os.system("color 2")
       os.system("echo \"HACK NASA MOD JK TRAVA\"")
